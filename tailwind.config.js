7bd61b595a644ac0253ade554eb4d94137c7190a
Tari <ummu.fasa1886@gmail.com>
2023-05-18 07:57:10 +0700

    pembuatan

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        utama: ["Chicle"],
+      },
+      backgroundImage: {
+        taman: "url('/src/img/taman.png')",
+      },
+    },
+  },
+  plugins: [],
+};
